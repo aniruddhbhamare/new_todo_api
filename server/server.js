6b98079ada6c4c601237cb4f21bfcafa1b600044
Aniruddha Bhamare <ani.fullstack@gmail.com>
2018-09-21 02:48:31 -0700

    added  users and todos post methods

---
+++ server/server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+app.use(bodyparser.json());
+
+app.post('/todos',(req,res)=>{
+  var todo = new Todo({
+      text:req.body.text,
+      completed:req.body.completed
+  });
+
+  todo.save().then((doc)=>{
+      res.status(200).send({doc});
+  },(err)=>{
+      res.status(400).send(err);
+  });
+})
+app.post('/users',(req,res)=>{
+    var user = new User({
+        name:req.body.name
+    });
+
+    user.save().then(
+    (doc)=>{
+        res.status(200).send(doc);
+    },
+    (err)=>{
+        res.status(400).send(err);
+    });
+});
+
+
+app.listen(3000,()=>{
+    console.log('server is running on port 3000');
+});
+
+// var newUser = new User({
+//     name:'user1'
+// });
+
+// newUser.save().then((doc)=>{console.log({doc})},(err)=>{console.log(err)});
