8dee4bc75798d48474c8b90afb7d00f52bea01c9
Aniruddha Bhamare <ani.fullstack@gmail.com>
2018-09-27 01:55:56 -0700

    added GET/users/me and middleware authentication

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 
 var app = express();
@@ -112,6 +113,20 @@ app.post('/users',(req,res)=>{
     }).catch((e)=>{
         res.status(400).send(e);
     })
+});
+
+
+app.get('/users/me',authenticate,(req,res)=>{
+    // var token = req.header('x-auth');
+    // User.findByToken(token).then((user)=>{
+    //     if(!user){
+    //         return Promise.reject();
+    //     }
+    //     res.send(user);
+    // }).catch((e)=>{
+    //     res.status(401).send();
+    // });
+    res.send(req.user);
 });
 
 app.get('/users',(req,res)=>{
